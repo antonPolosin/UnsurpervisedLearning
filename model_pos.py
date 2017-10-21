f3bd93d776d179cd4f55729bc649682e472af0a8
Nagarius <pooper2010@live.com.au>
2017-10-21 22:40:28 +1000

    Add files via upload

--- model_pos.py
+++ model_pos.py
@@ -2,6 +2,10 @@
 import pandas as pd
 import numpy as np
 from apyori import apriori
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def data_prep():
@@ -42,6 +46,27 @@ def result():
 	
 	result_df = result_df.sort_values(by='Lift', ascending=False)
 	print(result_df.head(10))
+	return result_df
+	
+def pairplot():	
+	results_df = result()
+	df2 = results_df[['Support', 'Confidence', 'Lift']]
+	scaler = StandardScaler()
+	
+	X = df2.as_matrix()
+	X = scaler.fit_transform(X)
+	
+	model = KMeans(n_clusters=3, random_state=42).fit(X)
+	y = model.predict(X)
+	df2['Cluster_ID'] = y
+	
+	# how many records are in each cluster
+	print("Cluster membership")
+	print(df2['Cluster_ID'].value_counts())
+
+	# pairplot the cluster distribution.
+	cluster_g = sns.pairplot(df2, hue='Cluster_ID')
+	plt.show()
 	
 def convert_apriori_results_to_pandas_df(results):
     rules = []
